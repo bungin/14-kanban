fc69b11ab67c494dc1bd8cc3f9e18c56a80f270c
bungin <nimai.band@gmail.com>
2024-11-07 22:57:54 -0600

    auth.ts and authAPI.tsx

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -2,29 +2,47 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 
 class AuthService {
   getProfile() {
-    // TODO: return the decoded token
+    // DO: return the decoded token
+    return jwtDecode(this.getToken());
   }
 
   loggedIn() {
-    // TODO: return a value that indicates if the user is logged in
+    // DO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    try {
+      const decoded = jwtDecode<JwtPayload>(token);
+      if (decoded?.exp && decoded?.exp < Date.now() / 1000) {
+        return true;
+      }
+    } catch (err) {
+      return false;
+    }
   }
 
   getToken(): string {
-    // TODO: return the token
+    // DO: return the token
+    const loggedUser = localStorage.getItem('token') || "";
+    return loggedUser;
   }
 
   login(idToken: string) {
-    // TODO: set the token to localStorage
-    // TODO: redirect to the home page
+    // DO: set the token to localStorage
+    localStorage.setItem('id_token', idToken);
+    // DO: redirect to the home page
+    window.location.assign('/');
   }
 
   logout() {
-    // TODO: remove the token from localStorage
-    // TODO: redirect to the login page
+    // DO: remove the token from localStorage
+    localStorage.removeItem('id_token');
+    // DO: redirect to the login page
+    window.location.assign('/');
+
   }
 }
 
