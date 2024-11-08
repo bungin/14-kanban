02574ccd8c82f99308c2d6b937f41702373d5908
bungin <nimai.band@gmail.com>
2024-11-07 23:24:00 -0600

    finish auth-routes and routes/index.ts auth

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -3,13 +3,41 @@ import { User } from '../models/user.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
-export const login = async (req: Request, res: Response) => {
-  // TODO: If the user exists and the password is correct, return a JWT token
-};
-
 const router = Router();
 
-// POST /login - Login a user
-router.post('/login', login);
 
-export default router;
+export const login = async (req: Request, res: Response) => {
+  // DO: If the user exists and the password is correct, return a JWT token
+  const { username, password } = req.body;
+  
+  try {
+    const user = await User.findOne({ where: { username } });
+    
+    if (!user) {
+      return res.status(404).json({ message: 'Auth failed' });
+    }
+    
+    const validPassword = await bcrypt.compare(password, user.password);
+    console.log("User or pass invalid");
+    
+    if (!validPassword) {
+      console.log("User or pass invalid");
+      return res.status(404).json({ message: 'Auth failed' });
+    }
+    
+    const secretKey = process.env.JWT_SECRET_KEY || "";
+    const token = jwt.sign({ username }, secretKey, { expiresIn: '2h' });
+    
+    console.log("User logged in: ", username);
+    return res.json({ token });
+  } catch (err) {
+    console.log("User login error: ", err);
+    return res.status(500).json({ message: 'Server Error', err });
+  }
+};
+  
+  
+  // POST /login - Login a user
+  router.post('/login', login);
+  
+  export default router;
